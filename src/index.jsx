3bea097f2a0cc7b1e1e6a4b4a7a83df173878a03
Brendan Thomas <brthomas@covermymeds.com>
2017-02-25 12:34:54 -0500

    hook up vote() and add action creators

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,7 @@ import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 import io from 'socket.io-client';
+import {setState} from './action_creators';
 
 require('./style.css');
 
@@ -15,7 +16,7 @@ const store = createStore(reducer);
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
-  store.dispatch({type: 'SET_STATE', state})
+  store.dispatch(setState(state))
 );
 
 const routes = <Route component={App}>
