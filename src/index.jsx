46e832da100ebd000f4e4a3f894be7b3cc1ae9e6
Brendan Thomas <brthomas@covermymeds.com>
2017-02-25 21:17:34 -0500

    connect client to server

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory } from 'react-router';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
 import App from './components/App';
@@ -9,15 +9,20 @@ import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 import io from 'socket.io-client';
 import {setState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
 
 require('./style.css');
-
-const store = createStore(reducer);
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
   store.dispatch(setState(state))
 );
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+
+const store = createStoreWithMiddleware(reducer);
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
